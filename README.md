35cf5025b055edd8cd88d656361732896e46cfb6
renovrtio <qin_anqi@foxmail.com>
2020-07-12 13:56:55 +0800

    调整使用生成器

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pdf2image
-pdf 转 image
+
+
+直接将.\dist\pdf2image.exe文件放在有pdf的文件夹中执行，会将此文件夹中所有的pdf文件转换成图片并存放在以pdf文件命名的文件夹中
--- pdf2image.py
+++ pdf2image.py
@@ -1,29 +1,38 @@
 import os
+from os import write
 import shutil
 import scandir
 import fitz
 
-if __name__ == '__main__':
-    dir_path = r'.'
+def wirte_image(filename):
+    doc = fitz.open(filename.path)
+    pg_num = doc.pageCount
+    fmt = f'{{0:0{len(str(pg_num))}d}}'
+    for pg in range(pg_num):
+        page = doc[pg]
+        page_num = fmt.format(pg + 1)
+        img_name = f'{filename.name.split(".")[0]}_{page_num}.jpg'
+        yield page,img_name
+
+def get_image(dir_path):
     os.scandir()
     filenames = os.scandir(dir_path)
     for filename in filenames:
         if filename.name.split('.')[-1] == 'pdf':
-            doc = fitz.open(filename.path)
-            pg_num = doc.pageCount
-            file_dir = os.path.join(dir_path, filename.name.split('.')[0])
-            if os.path.exists(file_dir):
-                shutil.rmtree(file_dir)
-            if pg_num > 1:
-                os.mkdir(file_dir)
-                for pg in range(pg_num):
-                    page = doc[pg]
-                    pm = page.getPixmap()
-                    pg_name = os.path.join(file_dir, str(pg + 1)) + '.jpg'
-                    pm.writeImage(pg_name)
-            else:
-                page = doc[0]
-                pm = page.getPixmap()
-                # pg_name = os.path.join(file_dir, str(pg + 1)) + '.jpg'
-                pm.writeImage(os.path.join(dir_path, filename.name.split('.')[0]) + '.jpg')
-                
+            # print(f'转转转换{filename.name},请稍等...')
+            yield filename
+
+if __name__ == '__main__':
+    dir_path = r'.'
+    for filename in get_image(dir_path):
+        print(f'正在转换{filename.name}，请稍等……')
+        file_dir = os.path.join(dir_path, filename.name.split('.')[0])
+        if not os.path.exists(file_dir):
+            # shutil.rmtree(file_dir)
+            os.mkdir(file_dir)
+        for page,imgname in wirte_image(filename):
+            pm = page.getPixmap()
+            pg_full_name = os.path.join(file_dir, dir_path, imgname)
+            print(f'图片提取到{pg_full_name}')
+            pm.writeImage( pg_full_name)
+    input('按任意键退出……')
